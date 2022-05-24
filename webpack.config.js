3801bc0c366c07c0f039c004cbedc60500e94963
Aryan Jain <ajhunny.jain@gmail.com>
2022-05-24 10:40:57 +0530

    Changed Package.json and build config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.resolve(__dirname, './src/index.js'),
   devtool: false,
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
     libraryTarget: 'umd',
-    library: 'artifactory-poc',
+    umdNamedDefine: true,
   },
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
+        test: /\.(js|jsx)?$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.(scss|css)$/,
@@ -36,4 +45,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new CleanWebpackPlugin()],
 };
